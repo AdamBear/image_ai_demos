d650a040a2a8fed61c2261c5419ba51cd3985bd7
AdamBear <adamxiong@126.com>
2022-08-19 15:14:38 +0800

    update gfpgan enhancement

--- image_enhance_gfpgan.py
+++ image_enhance_gfpgan.py
@@ -1,29 +1,24 @@
 import sys
-sys.path.insert(0, "/python/GFPGAN/")
-
-import argparse
-import cv2
-import glob
-import numpy as np
 import os
-import torch
+
+gfpgan_path = "/python/GFPGAN/"
+sys.path.insert(0, gfpgan_path)
+
+realesrganer_model_path = os.path.join(gfpgan_path, 'experiments/pretrained_models/RealESRGAN_x2plus.pth')
+gfpganer_model_path = os.path.join(gfpgan_path, 'experiments/pretrained_models/GFPGANCleanv1-NoCE-C2.pth')
+frames_path = "./upload"
+
 from basicsr.utils import imwrite
 
 from basicsr.archs.rrdbnet_arch import RRDBNet
 from realesrgan import RealESRGANer
 import cv2
-from facexlib.utils.face_restoration_helper import FaceRestoreHelper
+import glob
 
-from gfpgan.archs.gfpganv1_arch import GFPGANv1
-from gfpgan.archs.gfpganv1_clean_arch import GFPGANv1Clean
 from gfpgan.utils import GFPGANer
 
 arch = 'clean'
 channel_multiplier = 2
-
-realesrganer_model_path = '/python/GFPGAN/experiments/pretrained_models/RealESRGAN_x2plus.pth'
-gfpganer_model_path ='/python/GFPGAN/experiments/pretrained_models/GFPGANCleanv1-NoCE-C2.pth'
-
 
 model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
 bg_upsampler = RealESRGANer(
@@ -56,11 +51,6 @@ def restore_image(img_path, output_path):
     cropped_faces, restored_faces, restored_img = restorer.enhance(
         input_img, has_aligned=False, only_center_face=False, paste_back=True)
 
-    if restored_img is None:
-        print("enhanced failed!")
-    else:
-        print("enhanced success")
-
     # save faces
     for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
         # save cropped face
@@ -80,3 +70,84 @@ def restore_image(img_path, output_path):
 
     return save_restore_path
 
+
+def convert_frames_to_video(pathIn, pathOut, fps):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if os.path.isfile(os.path.join(pathIn, f))]
+    # for sorting the file names properly
+    files.sort(key=lambda x: int(x[5:-4]))
+    size2 = (0, 0)
+
+    for i in range(len(files)):
+        filename = pathIn + files[i]
+        # reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        size2 = size
+        print(filename)
+        # inserting the frames into an image array
+        frame_array.append(img)
+    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'DIVX'), fps, size2)
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+
+def convert_video_to_frames(f, frames_path):
+    # video to frames
+    cam = cv2.VideoCapture(str(f))
+
+    try:
+        # PATH TO STORE VIDEO FRAMES
+        if not os.path.exists(frames_path):
+            os.makedirs(frames_path)
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while (True):
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            name = os.path.join(frames_path, 'frame' + str(currentframe).zfill(8) + '.jpg')
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+            # print(currentframe)
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+
+
+def merge_audio(video_source_file, audio_source_file, target_file):
+    cmd = f"ffmpeg -i {video_source_file} -i {audio_source_file}  -c copy -map 0 -map 1:1 -y -shortest {target_file}"
+    ret = os.system(cmd)
+    if ret == 0:
+        return target_file
+    else:
+        return ""
+
+
+def restore_video(video_file, result_folder, fps=25):
+    convert_video_to_frames(video_file, result_folder)
+    img_list = sorted(glob.glob(os.path.join(result_folder, '*')))
+    for img_path in img_list:
+        restore_image(img_path, result_folder)
+
+    target_video_file = video_file + ".sr_.mp4"
+    target_mp4_file = video_file + ".sr.mp4"
+    convert_frames_to_video(os.path.join(result_folder, "restored_imgs"), target_video_file, fps)
+    merge_audio(target_video_file, video_file, target_mp4_file)
+    return target_mp4_file
